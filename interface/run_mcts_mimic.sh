96f0a20022491133b6e1e08f0705a30e9ac777aa
Guiliang <gla68@sfu.ca>
2020-08-03 17:18:16 -0700

    2020-08-03

--- interface/run_mcts_mimic.sh
+++ interface/run_mcts_mimic.sh
@@ -8,7 +8,7 @@ touch $log_dir
 for ((n=0;n<31;n++))
 do
     echo shell running round $n
-    python3 run_mimic_learner.py -a $aid -r $n -d $log_dir -m mcts -g $game -l $launch_time 2>&1 &
+    python3 run_mimic_learner.py -c $cpuct -a $aid -r $n -d $log_dir -m mcts -g $game -l $launch_time 2>&1 &
     process_id=$!
     wait $process_id
     echo shell finish running round $n
