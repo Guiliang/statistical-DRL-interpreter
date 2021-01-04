033d79e45579fb7ddd824c8e04d245d0285741f3
Guiliang <gla68@sfu.ca>
2021-01-04 00:28:20 -0500

    2021-01-03

--- cedar_running.sh
+++ cedar_running.sh
@@ -4,9 +4,9 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --account=def-functor
 cd /home/functor/scratch/Galen/project-DRL-Interpreter/statistical-DRL-interpreter/
-#virtualenv python3
+# virtualenv python3
 source sdl-venv-py37/bin/activate
-cat requirements.txt | xargs -n 1 pip install
+# cat requirements.txt | xargs -n 1 pip install
 cd /home/functor/scratch/Galen/project-DRL-Interpreter/statistical-DRL-interpreter/interface
 chmod 777 run_mcts_mimic.sh
 ./run_mcts_mimic.sh
